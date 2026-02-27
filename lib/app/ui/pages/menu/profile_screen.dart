65738ebc6a256d158cbb46576116d526f20730b2
ahmadazarnia <azarbonyad.ahmad@golrang.com>
2026-02-27 16:09:30 +0330

    chore: bump version to 3.0.0

--- lib/app/ui/pages/menu/profile_screen.dart
+++ lib/app/ui/pages/menu/profile_screen.dart
@@ -513,7 +513,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ],
                   ),
                   SizedBox(height: 12),
-                  Center(child: Text('v1.0.12', style: Theme.of(context).textTheme.titleSmall,))
+                  Center(child: Text('v3.0.0', style: Theme.of(context).textTheme.titleSmall,))
                 ],
               ),
             ),
